3b05157cb6859549d1f63314bcfd1239cd392950
Phokoos <83214849+Phokoos@users.noreply.github.com>
2023-04-01 18:04:57 +0000

    Deploying to gh-pages from @ Phokoos/goit-js-hw-11@622b5dd66e384c4d226a8cf498179ed2dc9671be 🚀

---
+++ index.174387d2.js
@@ -0,0 +1,2 @@
+console.log("cl from testTwo"),console.log("Priject works");
+//# sourceMappingURL=index.174387d2.js.map
---
+++ index.29b49abb.js
@@ -0,0 +1,2 @@
+console.log("cl from testTwo"),console.log("Priject works");
+//# sourceMappingURL=index.29b49abb.js.map
