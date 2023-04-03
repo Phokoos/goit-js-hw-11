b5c7de3c0130a0da7d6a0c90a17579e27be1c074
Phokoos <hulkovskij@gmail.com>
2023-04-03 21:33:45 +0300

    Added css, loader, infinit scrolling and some animations

--- src/js/galleryCreater.js
+++ src/js/galleryCreater.js
@@ -8,17 +8,50 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
-// import InfiniteScroll
-import InfiniteScroll from 'infinite-scroll';
 
 const galleryDivEl = document.querySelector(".gallery");
 const formEl = document.querySelector(".search-form");
 const formInputEl = document.querySelector(".search-form__input");
 caches;
-const loadMoreBtnEl = document.querySelector(".load-more");
+// const loadMoreBtnEl = document.querySelector(".load-more");
+const loaderEl = document.querySelector(".loader");
 
+
+// Vars for datas
 let latesInputValue;
+let loadMoreValue;
+let totalPhotoInWindow = 40;
+let totalCountOfPhotosFromApi;
 
+
+// Work with Infinite Scrolling
+
+
+function requestToApiAfterScrolling() {
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+		if (clientHeight + scrollTop >= scrollHeight) {
+			console.log("Requests to API");
+			loaderEl.classList.add('show');
+
+			anotherRequestsToApi(formInputEl.value, loadMoreValue);
+
+			// Page += 1;
+			console.log(`We load ${loadMoreValue} page for you`);
+			loadMoreValue += 1;
+			totalPhotoInWindow += 40;
+			
+		   if (totalPhotoInWindow >= totalCountOfPhotosFromApi) {
+				window.removeEventListener('scroll', requestToApiAfterScrolling);
+				return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+			}
+		}
+}
+
+
+function loadMoreInfiniteScrolling() {
+	window.addEventListener('scroll', requestToApiAfterScrolling);
+}
 
 formEl.addEventListener("submit", (action) => {
 	action.preventDefault();
@@ -27,14 +60,13 @@ formEl.addEventListener("submit", (action) => {
 	if (latesInputValue === formInputEl.value) {
 		return 
 	}
-	loadMoreBtnEl.style.display = "none";
+
+	window.removeEventListener('scroll', requestToApiAfterScrolling);
+
 	galleryDivEl.innerHTML = "";
 	latesInputValue = formInputEl.value;
 	
 	requestToApi(formInputEl.value, 1);
-
-	
-	
 })
 
 function requestToApi(name, page) {
@@ -45,7 +77,12 @@ function requestToApi(name, page) {
 			}
 			addMarkup(data.data.hits);
 			showTotalHitsMessage(data.data.totalHits);
-			loadMoreBtn(data);
+			// loadMoreInfiniteScrolling(data);
+			window.addEventListener('scroll', requestToApiAfterScrolling);
+			
+			loadMoreValue = 2;
+			totalPhotoInWindow = 40;
+			totalCountOfPhotosFromApi = data.data.totalHits;
 		})
 }
 
@@ -54,6 +91,7 @@ function anotherRequestsToApi(name, page) {
 		.then(data => {
 			addMarkup(data.data.hits);
 			scrollSmooth();
+			loaderEl.classList.remove('show');
 		})
 }
 
@@ -66,20 +104,24 @@ function addMarkup(photosArray) {
    				alt="${tags}"
    				width="100%"
    				loading="lazy"/></a>
-  			<div class="info">
-   			<p class="info-item">
-      		<b>${likes}</b>
-    			</p>
-    			<p class="info-item">
-      			<b>${views}</b>
-    			</p>
-    			<p class="info-item">
-      			<b>${comments}</b>
-    			</p>
-    			<p class="info-item">
-      			<b>${downloads}</b>
-    			</p>
-  			</div>
+  				<div class="info">
+   				<div class="info-item">
+						<b>Likes</b>
+						<p>${likes}</p>
+					</div>
+    				<div class="info-item">
+						<b>Views</b>
+						<p>${views}</p>
+    				</div>
+    				<div class="info-item">
+						<b>Comments</b>
+						<p>${comments}</p>
+    				</div>
+    				<div class="info-item">
+						<b>Downloads</b>
+						<p>${downloads}</p>
+    				</div>
+  				</div>
 		</div>`
 	).join("");
 	galleryDivEl.insertAdjacentHTML("beforeend", photosMarkup);
@@ -95,29 +137,6 @@ let gallery = new SimpleLightbox('.gallery a',{
 	captionPosition: 'bottom',
 	captionDelay: 250,
 });
-
-function loadMoreBtn(data) {
-	let loadMoreValue = 2;
-	let totalPhotoInWindow = 40;
-	loadMoreBtnEl.style.display = "block";
-
-	if (totalPhotoInWindow >= data.data.totalHits) {
-			loadMoreBtnEl.style.display = "none";
-			return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-		}
-
-	loadMoreBtnEl.addEventListener("click", () => {
-		
-		anotherRequestsToApi(formInputEl.value, loadMoreValue);
-		loadMoreValue += 1;
-		totalPhotoInWindow += 40;
-
-		if (totalPhotoInWindow >= data.data.totalHits) {
-			loadMoreBtnEl.style.display = "none";
-			return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-		}
-	});
-}
 
 function showTotalHitsMessage(totalHits) {
 	Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
